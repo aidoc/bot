7e5e375d733bf77de63c8757c0d465c48f3f1b32
Dave Snowdon <dave@davesnowdon.com>
2017-06-24 12:54:23 +0100

    added code from speech to text example

--- messages/index.js
+++ messages/index.js
@@ -3,6 +3,12 @@ var builder = require("botbuilder");
 var botbuilder_azure = require("botbuilder-azure");
 var path = require('path');
 
+var fs = require('fs');
+var needle = require('needle');
+var request = require('request');
+var url = require('url');
+var speechService = require('./speech-service.js');
+
 var useEmulator = (process.env.NODE_ENV == 'development');
 
 var connector = useEmulator ? new builder.ChatConnector() : new botbuilder_azure.BotServiceConnector({
@@ -10,13 +16,6 @@ var connector = useEmulator ? new builder.ChatConnector() : new botbuilder_azure
     appPassword: process.env['MicrosoftAppPassword'],
     stateEndpoint: process.env['BotStateEndpoint'],
     openIdMetadata: process.env['BotOpenIdMetadata']
-});
-
-var bot = new builder.UniversalBot(connector);
-bot.localePath(path.join(__dirname, './locale'));
-
-bot.dialog('/', function (session) {
-    session.send('You said ' + session.message.text);
 });
 
 if (useEmulator) {
@@ -28,5 +27,102 @@ if (useEmulator) {
     server.post('/api/messages', connector.listen());    
 } else {
     module.exports = { default: connector.listen() }
+    console.log("Running in Azure");
 }
 
+// Basic text echoing
+//var bot = new builder.UniversalBot(connector);
+//bot.localePath(path.join(__dirname, './locale'));
+//
+//bot.dialog('/', function (session) {
+//    session.send('You said ' + session.message.text);
+//});
+
+
+var bot = new builder.UniversalBot(connector, function (session) {
+    if (hasAudioAttachment(session)) {
+        var stream = getAudioStreamFromMessage(session.message);
+        speechService.getTextFromAudioStream(stream)
+            .then(function (text) {
+                session.send(processText(text));
+            })
+            .catch(function (error) {
+                session.send('Oops! Something went wrong. Try again later.');
+                console.error(error);
+            });
+    } else {
+        session.send('Did you upload an audio file? I\'m more of an audible person. Try sending me a wav file');
+    }
+});
+
+//=========================================================
+// Utilities
+//=========================================================
+function hasAudioAttachment(session) {
+    return session.message.attachments.length > 0 &&
+        (session.message.attachments[0].contentType === 'audio/wav' ||
+            session.message.attachments[0].contentType === 'application/octet-stream');
+}
+
+function getAudioStreamFromMessage(message) {
+    var headers = {};
+    var attachment = message.attachments[0];
+    if (checkRequiresToken(message)) {
+        // The Skype attachment URLs are secured by JwtToken,
+        // you should set the JwtToken of your bot as the authorization header for the GET request your bot initiates to fetch the image.
+        // https://github.com/Microsoft/BotBuilder/issues/662
+        connector.getAccessToken(function (error, token) {
+            var tok = token;
+            headers['Authorization'] = 'Bearer ' + token;
+            headers['Content-Type'] = 'application/octet-stream';
+
+            return needle.get(attachment.contentUrl, { headers: headers });
+        });
+    }
+
+    headers['Content-Type'] = attachment.contentType;
+    return needle.get(attachment.contentUrl, { headers: headers });
+}
+
+function checkRequiresToken(message) {
+    return message.source === 'skype' || message.source === 'msteams';
+}
+
+function processText(text) {
+    var result = 'You said: ' + text + '.';
+
+    if (text && text.length > 0) {
+        var wordCount = text.split(' ').filter(function (x) { return x; }).length;
+        result += '\n\nWord Count: ' + wordCount;
+
+        var characterCount = text.replace(/ /g, '').length;
+        result += '\n\nCharacter Count: ' + characterCount;
+
+        var spaceCount = text.split(' ').length - 1;
+        result += '\n\nSpace Count: ' + spaceCount;
+
+        var m = text.match(/[aeiou]/gi);
+        var vowelCount = m === null ? 0 : m.length;
+        result += '\n\nVowel Count: ' + vowelCount;
+    }
+
+    return result;
+}
+
+//=========================================================
+// Bots Events
+//=========================================================
+
+// Sends greeting message when the bot is first added to a conversation
+bot.on('conversationUpdate', function (message) {
+    if (message.membersAdded) {
+        message.membersAdded.forEach(function (identity) {
+            if (identity.id === message.address.bot.id) {
+                var reply = new builder.Message()
+                    .address(message.address)
+                    .text('Hi! I am SpeechToText Bot. I can understand the content of any audio and convert it to text. Try sending me a wav file.');
+                bot.send(reply);
+            }
+        });
+    }
+});
