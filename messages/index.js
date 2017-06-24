b7308b6ce787ffb9ec30f056320201b7dd7d05ff
Dave Snowdon <dave@davesnowdon.com>
2017-06-24 11:18:59 +0100

    trying with calling connector

--- messages/index.js
+++ messages/index.js
@@ -6,21 +6,24 @@ var path = require('path');
 
 var useEmulator = (process.env.NODE_ENV == 'development');
 
-var connector = useEmulator ? new builder.ChatConnector() : new botbuilder_azure.BotServiceConnector({
+//var connector = useEmulator ? new builder.ChatConnector() : new botbuilder_azure.BotServiceConnector({
+var connector = new calling.CallConnector({
     appId: process.env['MicrosoftAppId'],
     appPassword: process.env['MicrosoftAppPassword'],
     stateEndpoint: process.env['BotStateEndpoint'],
     openIdMetadata: process.env['BotOpenIdMetadata']
 });
 
+// text chatbot "hello world"
 //var bot = new builder.UniversalBot(connector);
-//bot.localePath(path.join(__dirname, './locale'));
-
 //bot.dialog('/', function (session) {
 //    session.send('You said ' + session.message.text);
 //});
 
 var bot = new calling.UniversalCallBot(connector);
+
+// not sure if this applies to bots in Azure
+//bot.localePath(path.join(__dirname, './locale'));
 //server.post('/api/calls', connector.listen());
 
 // Add root dialog
